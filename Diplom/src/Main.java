fb0d540c69103d9b441182c69a25b679cc8529a5
Cecilie <cille112@yahoo.dk>
2018-09-18 19:03:35 +0200

    Wuhuu det virker tror jeg :D :D

--- Diplom/src/Main.java
+++ Diplom/src/Main.java
@@ -1,65 +1,106 @@
+
 public class Main {
-	
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int [][] matrix = {{5,3,2,8},{7,9,2,6},{6,4,5,7},{5,7,7,8}};
-		int[][] matrixNew = new int[4][4];
-		int[][] matrixUnderway = new int[4][4];
-		boolean covered;
-		int small;
-		
-		
-		KuhnMunkres kuhn = new KuhnMunkres();
-		
-		matrixNew = kuhn.SmallestRow(matrix);
-		matrixNew = kuhn.SmallestColumn(matrixNew);
-		
-		for (int i = 0; i < matrixNew.length; i++) {
-			for (int j = 0; j < matrixNew.length; j++) {
-				System.out.print(matrixNew[i][j] + " ");
-			}
-			System.out.println();
+		int [][] matrix = {{7,5,6,9},{8,7,7,5},{3,1,0,9},{7,5,5,9}};
+		//int [][] matrix = {{1,0,0,1},{1,0,0,0},{0,0,0,1},{1,0,0,1}};
+		jobAssignment job = new jobAssignment(matrix);
+		//		int[][] matrixNew = new int[4][4];
+		//		int[][] matrixUnderway = new int[4][4];
+		//		boolean covered;
+		//		int small;
+		//		
+		//		
+		//		KuhnMunkres kuhn = new KuhnMunkres();
+		//		
+		//		matrixNew = kuhn.SmallestRow(matrix);
+		//		matrixNew = kuhn.SmallestColumn(matrixNew);
+		//		
+		//		for (int i = 0; i < matrixNew.length; i++) {
+		//			for (int j = 0; j < matrixNew.length; j++) {
+		//				System.out.print(matrixNew[i][j] + " ");
+		//			}
+		//			System.out.println();
+		//		}
+		//		
+		//		System.out.println();
+		//		
+		//		matrixUnderway = kuhn.markZeroes(matrixNew);
+		//		
+		//		for (int i = 0; i < matrixNew.length; i++) {
+		//			for (int j = 0; j < matrixNew[0].length; j++) {
+		//				System.out.print(matrixUnderway[i][j] + " ");
+		//			}
+		//			System.out.println();
+		//		}
+		//		
+		//		covered = kuhn.allZeroMarked(matrixNew, matrixUnderway);
+		//		System.out.println(covered);
+		//		
+		//		small = kuhn.findSmallest(matrixNew);
+		//		System.out.println(small);
+		//		
+		//		matrixNew = kuhn.createZero(matrixNew, matrixUnderway, small);
+		//		
+		//		for (int i = 0; i < matrixNew.length; i++) {
+		//			for (int j = 0; j < matrixNew.length; j++) {
+		//				System.out.print(matrixNew[i][j] + " ");
+		//			}
+		//			System.out.println();
+		//		}
+		//		
+		//		System.out.println();
+		//		
+		//		matrixUnderway = kuhn.markZeroes(matrixNew);
+		//		
+		//		for (int i = 0; i < matrixNew.length; i++) {
+		//			for (int j = 0; j < matrixNew[0].length; j++) {
+		//				System.out.print(matrixUnderway[i][j] + " ");
+		//			}
+		//			System.out.println();
+		//		}
+		//		
+		//		covered = kuhn.allZeroMarked(matrixNew, matrixUnderway);
+		//		System.out.println(covered);
+
+		job.initialzeAuxiliray();
+		job.makeEqualityGraph();
+		job.makeInitialMatching();
+		job.initialzeMarkingProcedure();
+		job.markingProcedure();
+		while(job.lengthPath != job.equalityGraph.length){
+			job.updateAuxilirayNumbers();	
+			job.resetMatching();
+			job.makeEqualityGraph();
+			job.makeInitialMatching();
+			job.initialzeMarkingProcedure();
+			job.markingProcedure();
 		}
-		
-		System.out.println();
-		
-		matrixUnderway = kuhn.markZeroes(matrixNew);
-		
-		for (int i = 0; i < matrixNew.length; i++) {
-			for (int j = 0; j < matrixNew[0].length; j++) {
-				System.out.print(matrixUnderway[i][j] + " ");
-			}
-			System.out.println();
+		job.printMatching();
+
+
+
+		for (int i = 0; i < matrix.length; i++) {
+			//System.out.println(job.sqaure[i]);
+			//System.out.println(job.triangle[i]);
+			//System.out.println(job.columnPicked[i]);
+			//System.out.println(job.auxiliaryNumbersRow[i]);
+			//System.out.println(job.auxiliaryNumbersColumn[i]);
+			//System.out.println(job.rowPicked[i]);
+			//System.out.println(job.pathNewColumn[i]);
+			//System.out.println(job.pathNewRow[i]);
 		}
-		
-		covered = kuhn.allZeroMarked(matrixNew, matrixUnderway);
-		System.out.println(covered);
-		
-		small = kuhn.findSmallest(matrixNew);
-		System.out.println(small);
-		
-		matrixNew = kuhn.createZero(matrixNew, matrixUnderway, small);
-		
-		for (int i = 0; i < matrixNew.length; i++) {
-			for (int j = 0; j < matrixNew.length; j++) {
-				System.out.print(matrixNew[i][j] + " ");
+
+		for (int f = 0; f < matrix.length; f++) {
+			for (int i = 0; i < matrix.length; i++) {
+				//System.out.print(job.equalityGraph[f][i]);
 			}
-			System.out.println();
+			//System.out.println();
 		}
-		
-		System.out.println();
-		
-		matrixUnderway = kuhn.markZeroes(matrixNew);
-		
-		for (int i = 0; i < matrixNew.length; i++) {
-			for (int j = 0; j < matrixNew[0].length; j++) {
-				System.out.print(matrixUnderway[i][j] + " ");
-			}
-			System.out.println();
-		}
-		
-		covered = kuhn.allZeroMarked(matrixNew, matrixUnderway);
-		System.out.println(covered);
+
+		//System.out.println(job.epsilon);
+		//System.out.println(job.lengthPath); 
+
 	}
 
 }
--- Diplom/src/jobAssignment.java
+++ Diplom/src/jobAssignment.java
@@ -1,19 +1,39 @@
-
 public class jobAssignment {
-	
+
 	int[][] orginalMatrix;
 	int[] auxiliaryNumbersRow;
 	int[] auxiliaryNumbersColumn;
-	
-	
+	int[][] equalityGraph;
+	int[] columnPicked;
+	int[] rowPicked;
+	int[] sqaure;
+	int[] triangle;
+	boolean change = true;
+	int epsilon = 100;
+	int lengthPath = 0;
+	int [] pathNewRow;
+	int [] pathNewColumn;
+
+
 	public jobAssignment(int[][]matrix){
 		this.orginalMatrix = matrix;
+		this.equalityGraph = new int[matrix.length][matrix.length];
+		this.columnPicked = new int[matrix.length];
+		this.rowPicked = new int[matrix.length];
+		this.sqaure = new int[matrix.length];
+		this.triangle = new int[matrix.length];
+		this.pathNewRow = new int[matrix.length];
+		this.pathNewColumn = new int [matrix.length];
 	}
-	
+
 	public void initialzeAuxiliray(){
 		auxiliaryNumbersColumn = new int[orginalMatrix.length];
 		auxiliaryNumbersRow = new int[orginalMatrix.length];
 		for (int i = 0; i < orginalMatrix.length; i++) {
+			columnPicked[i] = -1;
+			rowPicked[i] = -1;
+			pathNewRow[i] = -1;
+			pathNewColumn[i] = -1;
 			int biggestNumber = 0;
 			for (int j = 0; j < orginalMatrix.length; j++){
 				if (biggestNumber < orginalMatrix[i][j])
@@ -22,5 +42,154 @@ public class jobAssignment {
 			auxiliaryNumbersRow[i] = biggestNumber;
 		}		
 	}
+
+	public void makeEqualityGraph(){
+		for (int i = 0; i < orginalMatrix.length; i++) {
+			for (int j = 0; j < orginalMatrix.length; j++) {
+				if (orginalMatrix[i][j] == auxiliaryNumbersColumn[j] + auxiliaryNumbersRow[i])
+					equalityGraph[i][j] = 1;
+			}
+		}
+	}
+
+	public void makeInitialMatching(){
+		for (int i = 0; i < equalityGraph.length; i++) {
+			for (int j = 0; j < equalityGraph.length; j++) {
+				if (equalityGraph[i][j] == 1){
+					if(rowPicked[j] == -1){
+						columnPicked[i] = j;
+						rowPicked[j] = i;
+						lengthPath++;
+						break;
+					}
+				}
+
+			}
+		}
+	}
 	
+
+
+	public void initialzeMarkingProcedure(){
+		for (int i = 0; i < sqaure.length; i++) {
+			pathNewColumn[i] = -1;
+			pathNewRow[i] = -1;
+			if( columnPicked[i] == -1){
+				sqaure[i] = 1;
+			}
+		}
+	}
+
+	public void markingProcedure(){
+		change = true;
+		while(change){
+			change = false;
+			stepOne();
+		}
+	}
+
+	public void resetMatching(){
+		for (int i = 0; i < auxiliaryNumbersColumn.length; i++) {
+			rowPicked[i] = -1;
+			columnPicked[i] = -1;
+			sqaure[i] = 0;
+			triangle[i] = 0;
+		}
+		lengthPath = 0;
+	}
+	
+	public void printMatching(){
+		int price = 0;
+		for (int i = 0; i < auxiliaryNumbersColumn.length; i++) {
+			price = price + orginalMatrix[i][columnPicked[i]];
+		}
+		System.out.println(price);
+	}
+	
+	public void updateAuxilirayNumbers(){
+		for (int i = 0; i < sqaure.length; i++) {
+			if(sqaure[i] == 2){
+				for (int j = 0; j < triangle.length; j++) {
+					if(triangle[j] == 0){
+						int eps = (auxiliaryNumbersColumn[j] + auxiliaryNumbersRow[i])-orginalMatrix[i][j];
+						if(epsilon > eps){
+							epsilon = eps;
+						}
+					}
+				}
+			}
+		}
+		for (int i = 0; i < sqaure.length; i++) {
+			if(sqaure[i] == 2){
+				auxiliaryNumbersRow[i] = auxiliaryNumbersRow[i] - epsilon;
+			}
+			if(triangle[i] == 2){
+				auxiliaryNumbersColumn[i] = auxiliaryNumbersColumn[i] + epsilon;
+			}
+		}
+	}
+
+	private void stepOne(){
+		for (int i = 0; i < sqaure.length; i++) {
+			if (sqaure[i] == 1){
+				sqaure[i] = 2;
+				for (int j = 0; j < equalityGraph[i].length; j++) {
+					if(equalityGraph[i][j] == 1 && triangle[j] == 0){
+						triangle[j] = 1;
+						change = true;
+						pathNewColumn[i] = j;
+						stepTwo();
+					}
+				}
+			}
+		}
+	}
+
+	private void stepOne(int i){
+		sqaure[i] = 2;
+		for (int j = 0; j < sqaure.length; j++) {
+			if(equalityGraph[i][j] == 1 && triangle[j] == 0){
+				triangle[j] = 1;
+				change = true;
+				pathNewColumn[i] = j;
+				stepTwo();
+
+			}
+		}
+	}
+
+	private void stepTwo(){
+		for (int i = 0; i < triangle.length; i++) {
+			if(triangle[i] == 1){
+				if(rowPicked[i] != -1){
+					sqaure[rowPicked[i]] = 1;
+					triangle[i] = 2;
+					change = true;
+					pathNewRow[i] = rowPicked[i];
+					stepOne(i);
+				}
+				else{
+					changePath();
+					return;
+				}
+			}
+		}
+	}
+
+	private void changePath(){
+		for (int i = 0; i < pathNewColumn.length; i++) {
+			if(pathNewColumn[i] != -1){
+				columnPicked[i] = pathNewColumn[i];
+				rowPicked[pathNewColumn[i]] = i;
+			}
+			pathNewColumn[i] = -1;
+			pathNewRow[i] = -1;
+		}
+		lengthPath++;
+	}
+
 }
+
+
+
+
